02c843760071d39faad33963061dcced2429d7b4
Elizabeth Okorie <elizabethokorie407@gmail.com>
2023-10-06 15:36:04 +0100

    deploy

--- dist/controller/commentController.js
+++ dist/controller/commentController.js
@@ -2,7 +2,7 @@
 // import {Request , Response} from "express"
 // import {PrismaClient} from "@prisma/client"
 // const prisma = new PrismaClient();
-// export const makeComment = async(req : Request , res : Response) => {
+// export const createComment = async(req : Request , res : Response) => {
 //     try {
 //         const { userID , postID } = req.params
 //         const { comment } = req.body;
--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = parseInt(process.env.PORT);
 (0, mainApp_1.mainApp)(app);
-const server = app.listen(port, () => {
+const server = app.listen(process.env.PORT || port, () => {
     console.log("connected", port);
 });
 process.on("uncaughtException", (err) => {
